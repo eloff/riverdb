8bd3a5aeba0a802efd5b79c12e9794b366b2bc0f
Eloff <dan.eloff@gmail.com>
2021-11-02 13:59:10 +0100

    filling in normalize.rs, WIP

--- src/riverdb/pg/sql/normalize.rs
+++ src/riverdb/pg/sql/normalize.rs
@@ -37,60 +37,7 @@ impl<'a> QueryNormalizer<'a> {
             tags: Vec::new(),
         };
 
-        loop {
-            let c = this.next()?.get();
-            debug!("main loop c='{}'", c); // TODO char format code
-
-            let mut res = Ok(());
-            if is_whitespace(c) {
-                this.whitespace(c);
-            } else if c == '\'' {
-                res = this.single_quoted_string(c);
-            } else if c == '"' {
-                res = this.quoted_identifier(c);
-            } else if c == '$' {
-                res = this.maybe_dollar_string(c);
-            } else if c == '.' || is_digit(c) {
-                res = this.numeric(c);
-            } else if (c == 'N' || c == 'n') && this.match_fold("ull") {
-                res = this.null();
-            } else if (c == 'B' || c == 'b') && this.peek() == '\'' {
-                res = this.bit_string(c);
-            } else if (c == 'E' || c == 'e') && this.peek() == '\'' {
-                res = this.escape_string(c);
-            } else if (c == 'U' || c == 'u') && this.peek() == '&' {
-                res = this.unicode_string(c);
-            } else if (c == 'T' || c == 't') && this.match_fold("rue") {
-                res = this.bool(true);
-            } else if (c == 'F' || c == 'f') && this.match_fold("alse") {
-                res = this.bool(false);
-            } else if c == '/' && this.peek() == '*' {
-                res = this.c_style_comment(c);
-            } else if c == '-' && this.peek() == '-' {
-                res = this.sql_comment(c);
-            } else if is_letter(c) || c == '_' {
-                res = this.keyword_or_identifier(c);
-            } else if c == '(' || c == ')' || c == '[' || c == ']' || c == ',' {
-                this.append_byte(c as u8);
-            } else if c == ';' {
-                // Ignore ; if it occurs at the end of the query
-                this.whitespace(this.next()?);
-                if this.peek() == 0 {
-                    break
-                } else {
-                    // TODO verify this is correct and add test case for this too
-                    res = this.operator(c);
-                }
-            } else if c < 128 {
-                res = this.operator(c)
-            } else {
-                res = Err(Error::new(format!("unexpected char {} in query", c))); // TODO char format code
-            }
-
-            res?;
-        }
-
-        let query_type = QueryType::from(&this.normalized_query);
+        let query_type = this.do_normalize()?;
 
         return Ok(Query{
             normalized_query: this.normalized_query,
@@ -101,6 +48,63 @@ impl<'a> QueryNormalizer<'a> {
         })
     }
 
+    fn do_normalize(&mut self) -> Result<QueryType> {
+        loop {
+            let c = self.next()?.get();
+            debug!("main loop c='{}'", c); // TODO char format code
+
+            let mut res = Ok(());
+            if c.is_ascii_whitespace() {
+                self.consume_whitespace(c);
+            } else if c == '\'' {
+                res = self.single_quoted_string(c);
+            } else if c == '"' {
+                res = self.quoted_identifier(c);
+            } else if c == '$' {
+                res = self.maybe_dollar_string(c);
+            } else if c == '.' || c.is_ascii_digit() {
+                res = self.numeric(c);
+            } else if (c == 'N' || c == 'n') && self.match_fold("ull") {
+                res = self.null();
+            } else if (c == 'B' || c == 'b') && self.peek() == '\'' {
+                res = self.bit_string(c);
+            } else if (c == 'E' || c == 'e') && self.peek() == '\'' {
+                res = self.escape_string(c);
+            } else if (c == 'U' || c == 'u') && self.peek() == '&' {
+                res = self.unicode_string(c);
+            } else if (c == 'T' || c == 't') && self.match_fold("rue") {
+                res = self.bool(true);
+            } else if (c == 'F' || c == 'f') && self.match_fold("alse") {
+                res = self.bool(false);
+            } else if c == '/' && self.peek() == '*' {
+                res = self.c_style_comment(c);
+            } else if c == '-' && self.peek() == '-' {
+                res = self.sql_comment(c);
+            } else if c.is_alphabetic() || c == '_' {
+                res = self.keyword_or_identifier(c);
+            } else if c == '(' || c == ')' || c == '[' || c == ']' || c == ',' {
+                self.append_byte(c as u8);
+            } else if c == ';' {
+                // Ignore ; if it occurs at the end of the query
+                this.consume_whitespace(this.next()?);
+                if self.peek() == 0 {
+                    break
+                } else {
+                    // TODO verify this is correct and add test case for this too
+                    res = self.operator(c);
+                }
+            } else if c < 128 {
+                res = self.operator(c);
+            } else {
+                res = Err(Error::new(format!("unexpected char {} in query", c))); // TODO char format code
+            }
+
+            res?;
+        }
+
+        Ok(QueryType::from(&this.normalized_query))
+    }
+
     fn peek(&mut self) -> Result<char> {
         let (c, _) = util::decode_utf8_char(self.tail())?;
         Ok(c)
@@ -108,26 +112,32 @@ impl<'a> QueryNormalizer<'a> {
 
     fn next(&mut self) -> Result<char> {
         let (c, size) = util::decode_utf8_char(self.tail())?;
+        // TODO maybe we don't need this condition
+        if size != 0 {
+            self.last_char = c;
+            self.last_char_size = size;
+        }
         Ok(c)
     }
 
-    /// backup one character and return it. Panics if at start.
-    fn backup(&mut self) -> char {
+    /// backup one character. Panics if at start.
+    /// Can only be called exactly once after a call to next().
+    fn backup(&mut self) {
         assert!(self.pos != 0, "can't backup before start");
-        todo!()
+        assert!(self.last_char_size != 0, "must call next() before backup()");
+        debug_assert!(self.pos >= self.last_char_size);
+        self.pos -= self.last_char_size;
+        self.last_char_size = 0;
     }
 
     /// last returns the previously read character, without changing the position.
-    fn last(&mut self) -> char {
-        todo!()
+    fn last(&self) -> char {
+        self.last_char
     }
 
-    /// tail returns the remaining part of the string as a slice
-    fn tail(&mut self) -> &'a str {
-        unsafe {
-            // Safety: we already checked this is valid utf8 in the constructor
-            std::str::from_utf8_unchecked(&self.src[self.pos..])
-        }
+    /// tail returns the remaining part of the source bytes from the current position.
+    fn tail(&mut self) -> &'a [u8] {
+        &self.src[self.pos..]
     }
 
     /// matches str s at tail() case-insensitively
@@ -137,11 +147,18 @@ impl<'a> QueryNormalizer<'a> {
         if tail.len() < len {
             false
         } else {
-            &tail[..len].eq_ignore_ascii_case(s)
+            let mut i = 0;
+            for c in s {
+                if !tail.get(i).unwrap().eq_ignore_ascii_case(c) {
+                    return false;
+                }
+                i += 1;
+            }
+            true
         }
     }
 
-    // appends a space to the normalized query, if it doesn't end in TOKENS_WITHOUT_FOLLOWING_WHITESPACE
+    /// appends a space to the normalized query, if it doesn't end in TOKENS_WITHOUT_FOLLOWING_WHITESPACE
     fn write_space(&mut self) {
         if !self.normalized_query.is_empty() {
             let last_byte = self.normalized_query.as_bytes()[self.normalized_query.len()-1];
@@ -151,5 +168,79 @@ impl<'a> QueryNormalizer<'a> {
         }
     }
 
+    /// consumes all whitespace characters, starting with c
+    fn consume_whitespace(&mut self, c: char) {
+        while c.is_ascii_whitespace() {
+            c = self.next();
+        }
+        self.backup();
+    }
 
+    /// returns true if a string continuation is found immediately prior to pos
+    /// A string continuation is a ' followed by a newline, optionally followed by whitespace
+    fn look_behind_for_string_continuation(&self, pos: usize) -> bool {
+        let mut found_newline = false;
+        let mut i = pos - 1;
+        while i >= 0 {
+            let c = self.src.get(i).unwrap();
+            match c {
+                ' ' | '\t' | '\x0c' => (),
+                '\n' | '\r' => { found_newline = true; },
+                '\'' => return found_newline,
+                _ => return false,
+            }
+            i -= 1;
+        }
+        // unreachable because there *has* to be a preceding ' because we only call this if the last literal was a string literal
+        panic!("look_behind_for_string_continuation can only be called after finding a string literal");
+    }
+
+    fn replace_literal(&mut self, start: usize, ty: LiteralType, uppercase: bool) {
+        let tok = &self.src[start..self.pos];
+
+        // Any string except a dollar string may be combined with a plain string literal
+        // if separated with only whitespace including at least one newline.
+        if ty == LiteralType::String && !n.params.is_empty() {
+            let prev_param = self.params.last_mut();
+            match prev_param.ty {
+                LiteralType::String | LiteralType::EscapeString | LiteralType::UnicodeString | LiteralType::BitString => {
+                    // Check that there is only whitespace separating them, and it includes a newline
+                    if self.look_behind_for_string_continuation(start) {
+                        // Cut off the terminating single quote
+                        self.param_buf.pop().unwrap();
+                        // Append the string token, minus the starting single quote
+                        // Safety: We already decoded this as utf8.
+                        unsafe {
+                            self.param_buf.push_str(std::str::from_utf8_unchecked(&tok[1..]))
+                        }
+                        return
+                    }
+                },
+                _ = (),
+            }
+        }
+
+        let mut negated = false;
+        if ty == LiteralType::Integer || ty == LiteralType::Numeric {
+            negated = self.is_negative_number(start, self.pos);
+            // Remove the - from the end of the normalized string
+            if negated {
+                let trim = self.no
+            }
+        }
+    }
+
+    /// appends a NULL literal to params
+    fn null(&mut self) {
+        self.replace_literal(self.pos - 4, LiteralType::Null, true);
+    }
+
+    /// appends a TRUE or FALSE literal to params depending on the value of b.
+    fn bool(&mut self, b: bool) {
+        let mut start = self.pos - 5;
+        if b {
+            start += 1;
+        }
+        n.replace_literal(start, LiteralType::Boolean, true);
+    }
 }
